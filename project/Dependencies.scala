2fbbf5a2ba4ca0e454e0037d1e074e2a51d7c67c
Krzysztof Pado <povder@gmail.com>
2016-09-20 21:05:26 +0200

    Adaptations to API reorganization

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,18 @@
 import sbt._
 
+object Version {
+  val rdbc = "0.0.6"
+}
+
 object Library {
-  val rdbcCore = "io.rdbc" %% "rdbc-core" % "0.0.4"
+  val rdbcScalaApi = "io.rdbc" %% "rdbc-api-scala" % Version.rdbc
+  val rdbcImplbase = "io.rdbc" %% "rdbc-implbase" % Version.rdbc
+  val rdbcTypeconv = "io.rdbc" %% "rdbc-typeconv" % Version.rdbc
   val reactiveStreams = "org.reactivestreams" % "reactive-streams" % "1.0.0"
   val scodecBits = "org.scodec" %% "scodec-bits" % "1.1.0"
   val scodecCore = "org.scodec" %% "scodec-core" % "1.10.2"
   val scodecAkka = "org.scodec" %% "scodec-akka" % "0.2.0"
   val typesafeConfig = "com.typesafe" % "config" % "1.3.0"
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.4.10"
+  val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.4.10"
 }
--- rdbc-pgsql-akka/src/main/scala/io/rdbc/pgsql/PgStatement.scala
+++ rdbc-pgsql-akka/src/main/scala/io/rdbc/pgsql/PgStatement.scala
@@ -18,20 +18,22 @@ package io.rdbc.pgsql
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import io.rdbc.core.ImmutIndexedSeq
-import io.rdbc.core.api.Statement
-import io.rdbc.core.api.exceptions.BindException.{MissingParamValException, NoSuitableConverterFoundException}
+import io.rdbc.ImmutIndexedSeq
+import io.rdbc.api.exceptions.BindException.MissingParamValException
+import io.rdbc.api.exceptions.NoSuitableConverterFoundException
+import io.rdbc.implbase.BindablePartialImpl
 import io.rdbc.pgsql.core.PgTypeConvRegistry
 import io.rdbc.pgsql.core.messages.frontend._
+import io.rdbc.sapi.{ParametrizedStatement, Statement, TypeConverterRegistry}
 import org.reactivestreams.Publisher
 
 import scala.concurrent.Future
 
 case class PgNativeStatement(statement: String, params: ImmutIndexedSeq[String])
 
-trait PgStatement extends Statement {
+trait PgStatement extends Statement with BindablePartialImpl[ParametrizedStatement] {
 
-  def typeConvRegistry: PgTypeConvRegistry
+  def pgTypeConvRegistry: PgTypeConvRegistry
   def nativeStmt: PgNativeStatement
   protected def parametrizedStmt(dbValues: ImmutIndexedSeq[DbValue]): PgParametrizedStatement
   def sessionRef: SessionRef
@@ -40,7 +42,7 @@ trait PgStatement extends Statement {
 
   def convertParams(params: Map[String, Any]): Map[String, DbValue] = params.map { anyParamEntry =>
     val (anyParamName, anyParamValue) = anyParamEntry
-    typeConvRegistry.byClass(anyParamValue.getClass)
+    pgTypeConvRegistry.byClass(anyParamValue.getClass)
       .map(converter => {
         val strVal = converter.toPgTextual(anyParamValue)
         (anyParamName, TextualDbValue(strVal): DbValue)
@@ -67,52 +69,58 @@ trait PgStatement extends Statement {
 
   override def streamParams(paramsPublisher: Publisher[Map[String, Any]]): Future[Unit] = {
     //TODO parse first
-/*
-    //Parse(stmtName, sql, List.empty)
-    //, Bind(stmtName.map(_ + "_portal"), stmtName, params.toList, AllTextual)) //TODO toList, //TODO AllTextual
+    /*
+        //Parse(stmtName, sql, List.empty)
+        //, Bind(stmtName.map(_ + "_portal"), stmtName, params.toList, AllTextual)) //TODO toList, //TODO AllTextual
 
-    //sessionRef.actor ! Parse
+        //sessionRef.actor ! Parse
 
-    val src: Source[Bind, NotUsed] = Source.fromPublisher(paramsPublisher).map { params =>
-      //TODO massive code dupl
+        val src: Source[Bind, NotUsed] = Source.fromPublisher(paramsPublisher).map { params =>
+          //TODO massive code dupl
 
-      val errorsOrDbValues: ImmutSeq[BindEx Xor (String, DbValue)] = convertParams(params)
-      val errorOrDbValues: CompositeBindEx Xor Map[String, DbValue] = foldConvertedParams(errorsOrDbValues)
-      val errorOrIndexedDbValues: Xor[BindEx, Vector[DbValue]] = errorOrDbValues.flatMap { providedParams =>
-        nativeStmt.params.foldLeft(Xor.right[BindEx, Vector[DbValue]](Vector.empty[DbValue])) { (xorAcc, paramName) =>
-          xorAcc.flatMap { acc =>
-            providedParams.get(paramName) match {
-              case Some(paramValue) => Xor.right(acc :+ paramValue)
-              case None => Xor.left(MissingParamValEx(paramName))
+          val errorsOrDbValues: ImmutSeq[BindEx Xor (String, DbValue)] = convertParams(params)
+          val errorOrDbValues: CompositeBindEx Xor Map[String, DbValue] = foldConvertedParams(errorsOrDbValues)
+          val errorOrIndexedDbValues: Xor[BindEx, Vector[DbValue]] = errorOrDbValues.flatMap { providedParams =>
+            nativeStmt.params.foldLeft(Xor.right[BindEx, Vector[DbValue]](Vector.empty[DbValue])) { (xorAcc, paramName) =>
+              xorAcc.flatMap { acc =>
+                providedParams.get(paramName) match {
+                  case Some(paramValue) => Xor.right(acc :+ paramValue)
+                  case None => Xor.left(MissingParamValEx(paramName))
+                }
+              }
             }
           }
+
+          val dbParams = errorOrIndexedDbValues.getOrElse(???) //TODO
+
+          Bind(Some("STREAMING_P"), Some("STREAMING_S"), dbParams.toList, AllTextual)
         }
-      }
 
-      val dbParams = errorOrIndexedDbValues.getOrElse(???) //TODO
+        sessionRef.actor ! SubscribeBinds(src)
 
-      Bind(Some("STREAMING_P"), Some("STREAMING_S"), dbParams.toList, AllTextual)
-    }
-
-    sessionRef.actor ! SubscribeBinds(src)
-
-    XorF(Xor.right())
-    */
+        XorF(Xor.right())
+        */
     ???
   }
 
 }
 
-class CachedPgStatement(val stmtName: String, val nativeStmt: PgNativeStatement, val typeConvRegistry: PgTypeConvRegistry)(implicit val sessionRef: SessionRef, system: ActorSystem, materializer: ActorMaterializer) extends PgStatement {
+class CachedPgStatement(val stmtName: String,
+                        val nativeStmt: PgNativeStatement,
+                        val rdbcTypeConvRegistry: TypeConverterRegistry,
+                        val pgTypeConvRegistry: PgTypeConvRegistry)(implicit val sessionRef: SessionRef, system: ActorSystem, materializer: ActorMaterializer) extends PgStatement {
 
   override protected def parametrizedStmt(dbValues: ImmutIndexedSeq[DbValue]): PgParametrizedStatement = {
-    new CachedPgParametrizedStatement(stmtName, dbValues, typeConvRegistry)
+    new CachedPgParametrizedStatement(stmtName, dbValues, rdbcTypeConvRegistry, pgTypeConvRegistry)
   }
 }
 
-class NotCachedPgStatement(val nativeStmt: PgNativeStatement, val stmtName: Option[String], val typeConvRegistry: PgTypeConvRegistry)(implicit val sessionRef: SessionRef, system: ActorSystem, materializer: ActorMaterializer) extends PgStatement {
+class NotCachedPgStatement(val nativeStmt: PgNativeStatement,
+                           val stmtName: Option[String],
+                           val rdbcTypeConvRegistry: TypeConverterRegistry,
+                           val pgTypeConvRegistry: PgTypeConvRegistry)(implicit val sessionRef: SessionRef, system: ActorSystem, materializer: ActorMaterializer) extends PgStatement {
 
   override protected def parametrizedStmt(dbValues: ImmutIndexedSeq[DbValue]): PgParametrizedStatement = {
-    new NotCachedPgParametrizedStatement(nativeSql, stmtName, dbValues, typeConvRegistry)
+    new NotCachedPgParametrizedStatement(nativeSql, stmtName, dbValues, rdbcTypeConvRegistry, pgTypeConvRegistry)
   }
 }
